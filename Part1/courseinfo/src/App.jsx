27e5a61861446aada26c08b2d66aa58b4dadaed6
tianleigao <tianleigao@uvic.ca>
2025-12-01 21:38:57 -0800

    exercise 1.1-1.2

--- Part1/courseinfo/src/App.jsx
+++ Part1/courseinfo/src/App.jsx
@@ -1,3 +1,30 @@
+const Header = ({name}) => {
+  return <h1>{name}</h1>
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ parts }) => {
+  return (
+    <div>
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
+    </div>
+  )
+}
+
+const Total = ({ parts }) => {
+  const total = parts[0].exercises + parts[1].exercises + parts[2].exercises
+  return <p>Number of exercises {total}</p>
+}
+
 const App = () => {
   const course = {
     name: "Half Stack application development",
@@ -21,7 +48,7 @@ const App = () => {
     <div>
       <Header name={course.name} />
       <Content parts={course.parts} />
-      <Total exercises={course.exercises} />
+      <Total parts={course.parts} />
     </div>
   )
 }
